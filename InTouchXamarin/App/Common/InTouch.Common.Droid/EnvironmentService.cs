92d64a87c3ac1d5c94204e52ba2a1d9dc6f939c9
Zhixia Zhou <zhixia.zhou@connet.qut.edu.au>
2016-11-01 23:07:27 +1000

    Added Xamarin beta

---
+++ InTouchXamarin/App/Common/InTouch.Common.Droid/EnvironmentService.cs
@@ -0,0 +1,20 @@
+﻿using InTouch.Abstractions;
+using Android.OS;
+
+namespace InTouch.Common.Droid
+{
+    public class EnvironmentService : IEnvironmentService
+    {
+        #region IEnvironmentService implementation
+        public bool IsRealDevice
+        {
+            get
+            {
+                string f = Build.Fingerprint;
+                return !(f.Contains("vbox") || f.Contains("generic") || f.Contains("vsemu"));
+            }
+        }
+        #endregion
+    }
+}
+
---
+++ InTouchXamarin/App/Common/InTouch.Util/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace InTouch.Util
+{
+	public static class StringExtensions
+	{
+		public static string SanitizePhoneNumber(this string value)
+		{
+			return new string(value.ToCharArray().Where(char.IsDigit).ToArray());
+		}
+
+		public static bool IsNullOrWhiteSpace(this string value) 
+		{
+			return string.IsNullOrWhiteSpace(value);
+		}
+	}
+}
+
